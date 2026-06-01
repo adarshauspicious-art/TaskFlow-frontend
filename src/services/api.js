cdf20b861185c2bbc8317b70c5f3f1e65b314ef1
Adarsh Sharma Nadda <adarshauspicious@gmail.com>
2026-06-01 16:37:24 +0530

    render link for the backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskflow-backend-z2a8.onrender.com/api",
+  baseURL: "https://taskflow-backend-z2a8.onrender.com",
   withCredentials: true,
 });
 
-export default API;
+export default API; 
